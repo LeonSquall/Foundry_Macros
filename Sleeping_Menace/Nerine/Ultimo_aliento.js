c2d43ef35094ae44ad3942a1e6297df28149e3f1
Miguel Ángel López Cobo <m_a_lopez_c@hotmail.com>
2023-06-20 08:24:34 +0200

    Fix Last Breath

--- Sleeping_Menace/Nerine/Ultimo_aliento.js
+++ Sleeping_Menace/Nerine/Ultimo_aliento.js
@@ -1,5 +1,20 @@
 let actor = await game.actors.get("qBj6AzpcRyB06dD7"); // Nerine
 
+let effectToFind = "Último Aliento";
+let moduleToFind = "Custom Convenient Effects";
+let effect = null;
+
+for (let i = 0; i < game.data.items.length; i++) {
+  if (game.data.items[i].name === moduleToFind ) {
+    for (let j = 0; j < game.data.items[i].effects.length; j++) {
+      if (game.data.items[i].effects[j].label === effectToFind ) {
+        effect = game.data.items[i].effects[j];
+      }
+    }
+  }
+}
+
+console.log(game.data)
 if (actor.system.attributes.hp.value < 1) {
   let item = actor.items.find(i => i.id === "YXpBkl0fhyIEOj3d"); // Bastón del Génesis
 
@@ -9,24 +24,7 @@ if (actor.system.attributes.hp.value < 1) {
     await item.update({ "flags.magicitems.uses":  0});
 
     await actor.update({"system.attributes.hp.value": healing});
-
-    const effectData = {
-      label: "Último Aliento",
-      icon: "icons/magic/nature/elemental-plant-humanoid.webp",
-      changes: [
-        {
-          key: "data.abilities.wis.value",
-          mode: 2, // ADD
-          value: 4,
-        },
-      ],
-      duration: {
-        seconds: 60,
-      },
-      origin: `${item.data.name}`,
-    };
-
-    await actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+    await actor.createEmbeddedDocuments("ActiveEffect", [effect]);
 
     ChatMessage.create({
       content: `¡Último Aliento! Nerine recupera ${healing} puntos de vida y gana +4 de Sabiduría durante 60 segundos.`,
